bb1096b42aa91bae65d3cb912e9232ad226dc91d
Irish John De Roxas <irishderoxas@gmail.com>
2023-08-18 20:22:48 +0800

    Basic setup complete

---
+++ static/RPS.js
@@ -0,0 +1,4 @@
+// TODO check what the player clicks. Rock Paper or scissor
+// TODO function that randomize the select of the computer
+// TODO display Score/Sign and if its a tie 
+// TODO if first to 5 wins, show the modal after that and the buttons should show the modal if the score is 5
